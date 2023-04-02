fe84662596dc57358a04e62466778d996f4941d9
ishine <1263859380@qq.com>
2023-04-02 12:38:35 +0800

    Initial commit

---
+++ Dataset.py
@@ -0,0 +1,60 @@
+from torch.utils.data import Dataset
+import torch
+import cv2
+from IPython import display
+from matplotlib import pyplot as plt
+import os
+
+
+def CreateDataset(datafile):
+    frames = []
+    width, height = 512, 512
+    for root, dirs, files in os.walk(datafile):
+        print(root)
+        for file in files:
+            path = os.path.join(root, file)
+            print(path)
+
+            # 打开mp4文件
+            video = cv2.VideoCapture(path)
+
+            # 读取每一帧图像并将其转换为PyTorch张量
+            while True:
+                ret, frame = video.read()
+                if not ret:
+                    break
+                frame = cv2.resize(frame, (width, height))
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # 转换为RGB模式
+                frame = torch.from_numpy(frame).permute(2, 0, 1).float() / 255.0  # 转换为PyTorch张量
+                frames.append(frame)
+
+    # 将张量列表堆叠成一个张量
+    tensor = torch.stack(frames)
+
+    print(tensor.shape)
+
+    return MP4Dataset(tensor), torch.stack([tensor[0]])
+
+
+class MP4Dataset(Dataset):
+    def __init__(self, data):
+        self.datas = []
+        for i in range(data.shape[0] - 1):
+            self.datas.append((data[i], data[i + 1]))
+
+    def __len__(self):
+        return len(self.datas)
+
+    def __getitem__(self, item):
+        return self.datas[item]
+
+
+def use_svg_display():
+    # 用矢量图显示
+    display.set_matplotlib_formats('svg')
+
+
+def set_figsize(figsize=(3.5, 2.5)):
+    use_svg_display()
+    # 设置图的尺寸
+    plt.rcParams['figure.figsize'] = figsize
---
+++ net.py
@@ -0,0 +1,88 @@
+import torch
+from torch import nn, optim
+from torchvision import transforms
+
+
+class Autoencoder(nn.Module):
+    def __init__(self):
+        super(Autoencoder, self).__init__()
+
+        # 编码器
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(16, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        # 解码器
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(32, 16, kernel_size=2, stride=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(16, 3, kernel_size=2, stride=2),
+            nn.Sigmoid()  # 将输出限制在 [0, 1] 范围内
+        )
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
+
+
+def train(model, train_loader, test_loader, pre_frame, lr=1e-3, num_epochs=100, device='cpu'):
+    print(f'train on {device}')
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    unloader = transforms.ToPILImage()
+    l = nn.MSELoss()
+    train_loss = []
+    test_loss = []
+
+    for epoch in range(num_epochs):
+        running_loss = 0
+        for i, data in enumerate(train_loader):
+            inputs, outputs = data
+            inputs = inputs.to(device)
+
+            optimizer.zero_grad()
+            outputs_bar = model(inputs)
+            loss = l(outputs_bar, outputs)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+        epoch_loss = running_loss / len(train_loader)
+        now_test_loss = evaluate(model, test_loader, l)
+        train_loss.append(epoch_loss)
+        test_loss.append(now_test_loss)
+        if (epoch+1) % 20 == 0:
+            print(f"Epoch {epoch+1}/{num_epochs}: train loss: {epoch_loss:.4f}, test loss: {now_test_loss:.4f}")
+
+            preview_frame = create_next_frame(model, pre_frame, unloader)
+            preview_frame.save(f'outputs/{epoch+1} output.jpg')
+
+    return train_loss, test_loss
+
+
+def evaluate(model, test_loader, l, device='cpu'):
+    testing_loss = 0
+    for i, data in enumerate(test_loader):
+        inputs, outputs = data
+        inputs.to(device)
+
+        with torch.no_grad():
+            outputs_bar = model(inputs)
+            loss = l(outputs_bar, outputs)
+
+            testing_loss += loss
+
+    epoch_loss = testing_loss / len(test_loader)
+    return epoch_loss
+
+
+def create_next_frame(model, pre_frame, transform):
+    next_frame = model(pre_frame) * 255.0
+    image = transform(next_frame[0].cpu().clone())
+    return image
