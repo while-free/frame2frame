c69e114039397a28802102dc4b6f7ea3ecab3b41
ishine <1263859380@qq.com>
2023-04-03 12:35:19 +0800

    改了数据集生成

--- Dataset.py
+++ Dataset.py
@@ -4,10 +4,12 @@ import cv2
 from IPython import display
 from matplotlib import pyplot as plt
 import os
+import pickle
 
 
 def CreateDataset(datafile):
     frames = []
+    len_frames = [0]
     width, height = 512, 512
     for root, dirs, files in os.walk(datafile):
         print(root)
@@ -28,25 +30,43 @@ def CreateDataset(datafile):
                 frame = torch.from_numpy(frame).permute(2, 0, 1).float() / 255.0  # 转换为PyTorch张量
                 frames.append(frame)
 
+            len_frames.append(len(frames))
+
     # 将张量列表堆叠成一个张量
     tensor = torch.stack(frames)
 
-    print(tensor.shape)
+    dataset = MP4Dataset(tensor, len_frames)
+    print(f'length of dataset: {len(dataset)}')
 
-    return MP4Dataset(tensor), torch.stack([tensor[0]])
+    return dataset, torch.stack([tensor[0]])
 
 
 class MP4Dataset(Dataset):
-    def __init__(self, data):
+    def __init__(self, data, len_frames):
         self.datas = []
-        for i in range(data.shape[0] - 1):
-            self.datas.append((data[i], data[i + 1]))
+        for i in range(len(len_frames) - 1):
+            for j in range(len_frames[i], len_frames[i+1] - 1):
+                self.datas.append((data[j], data[j+1]))
 
     def __len__(self):
         return len(self.datas)
 
     def __getitem__(self, item):
         return self.datas[item]
+
+
+def save_dataset(myDataset, path):
+    """
+    path: end with '.pkl'
+    """
+    with open(path, "wb") as f:
+        pickle.dump(myDataset, f)
+
+
+def read_dataset(path, myDataset=None):
+    with open("example.pkl", "wb") as f:
+        pickle.dump(myDataset, f)
+    return myDataset
 
 
 def use_svg_display():
