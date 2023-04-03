50d2e07b2d405710e5817bd6488991112b5742f8
ishine <1263859380@qq.com>
2023-04-03 23:36:36 +0800

    改net

--- net.py
+++ net.py
@@ -33,6 +33,9 @@ class Autoencoder(nn.Module):
 
 def train(model, train_loader, test_loader, pre_frame, lr=1e-3, num_epochs=100, device='cpu'):
     print(f'train on {device}')
+
+    model = model.to(device)
+
     optimizer = optim.Adam(model.parameters(), lr=lr)
     unloader = transforms.ToPILImage()
     l = nn.MSELoss()
@@ -44,6 +47,7 @@ def train(model, train_loader, test_loader, pre_frame, lr=1e-3, num_epochs=100, 
         for i, data in enumerate(train_loader):
             inputs, outputs = data
             inputs = inputs.to(device)
+            outputs = outputs.to(device)
 
             optimizer.zero_grad()
             outputs_bar = model(inputs)
@@ -54,14 +58,14 @@ def train(model, train_loader, test_loader, pre_frame, lr=1e-3, num_epochs=100, 
             running_loss += loss.item()
 
         epoch_loss = running_loss / len(train_loader)
-        now_test_loss = evaluate(model, test_loader, l)
+        now_test_loss = evaluate(model, test_loader, l, device)
         train_loss.append(epoch_loss)
         test_loss.append(now_test_loss)
         if (epoch+1) % 20 == 0:
             print(f"Epoch {epoch+1}/{num_epochs}: train loss: {epoch_loss:.4f}, test loss: {now_test_loss:.4f}")
 
-            preview_frame = create_next_frame(model, pre_frame, unloader)
-            preview_frame.save(f'outputs/{epoch+1} output.jpg')
+           # preview_frame = create_next_frame(model, pre_frame, unloader)
+           # preview_frame.save(f'outputs/{epoch+1} output.jpg')
 
     return train_loss, test_loss
 
@@ -71,6 +75,7 @@ def evaluate(model, test_loader, l, device='cpu'):
     for i, data in enumerate(test_loader):
         inputs, outputs = data
         inputs.to(device)
+        outputs.to(device)
 
         with torch.no_grad():
             outputs_bar = model(inputs)
