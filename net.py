3ffa1d840c5a1588f9aa8cc516bb4dc2ccdf5919
ishine <1263859380@qq.com>
2023-04-07 21:10:21 +0800

    GAN

--- net.py
+++ net.py
@@ -1,96 +1,136 @@
 import torch
-from torch import nn, optim
-from torchvision import transforms
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torchvision.utils import save_image
+import numpy as np
 
 
-class Autoencoder(nn.Module):
+class Generator(nn.Module):
+    def __init__(self, latent_dim=100):
+        super(Generator, self).__init__()
+
+        self.latent_dim = latent_dim
+
+        self.model = nn.Sequential(
+            nn.Linear(self.latent_dim, 256),
+            nn.LeakyReLU(0.2),
+            nn.BatchNorm1d(256),
+            nn.Linear(256, 512),
+            nn.LeakyReLU(0.2),
+            nn.BatchNorm1d(512),
+            nn.Linear(512, 1024),
+            nn.LeakyReLU(0.2),
+            nn.BatchNorm1d(1024),
+            nn.Linear(1024, 28 * 28),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        img = self.model(z)
+        img = img.view(img.size(0), 1, 28, 28)
+        return img
+
+
+class Discriminator(nn.Module):
     def __init__(self):
-        super(Autoencoder, self).__init__()
+        super(Discriminator, self).__init__()
 
-        # 编码器
-        self.encoder = nn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(16, 32, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
+        self.model = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(28 * 28, 512),
+            nn.LeakyReLU(0.2),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
         )
 
-        # 解码器
-        self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(32, 16, kernel_size=2, stride=2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(16, 3, kernel_size=2, stride=2),
-            nn.Sigmoid()  # 将输出限制在 [0, 1] 范围内
-        )
-
-    def forward(self, x):
-        encoded = self.encoder(x)
-        decoded = self.decoder(encoded)
-        return decoded
+    def forward(self, img):
+        validity = self.model(img)
+        return validity
 
 
-def train(model, train_loader, test_loader, pre_frame, lr=1e-3, num_epochs=100, device='cpu'):
-    print(f'train on {device}')
+class GAN:
+    def __init__(self, lr=0.0002, betas=(0.5, 0.999)):
+        self.img_shape = (1, 28, 28)
 
-    model = model.to(device)
+        self.latent_dim = 100
 
-    optimizer = optim.Adam(model.parameters(), lr=lr)
-    unloader = transforms.ToPILImage()
-    l = nn.MSELoss()
-    train_loss = []
-    test_loss = []
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    for epoch in range(num_epochs):
-        running_loss = 0
-        for i, data in enumerate(train_loader):
-            inputs, outputs = data
-            inputs = inputs.to(device)
-            outputs = outputs.to(device)
+        # Build and compile the discriminator
+        self.discriminator = Discriminator().to(self.device)
+        self.optim_d = optim.Adam(self.discriminator.parameters(), lr=lr, betas=betas)
 
-            optimizer.zero_grad()
-            outputs_bar = model(inputs)
-            loss = l(outputs_bar, outputs)
-            loss.backward()
-            optimizer.step()
+        # Build the generator
+        self.generator = Generator(self.latent_dim).to(self.device)
 
-            running_loss += loss.item()
+        # The combined model we will only train the generator
+        self.discriminator.eval()
 
-        epoch_loss = running_loss / len(train_loader)
-        now_test_loss = evaluate(model, test_loader, l, device)
-        train_loss.append(epoch_loss)
-        test_loss.append(now_test_loss)
-        if (epoch+1) % 20 == 0:
-            print(f"Epoch {epoch+1}/{num_epochs}: train loss: {epoch_loss:.4f}, test loss: {now_test_loss:.4f}")
+        # The combined model  (stacked generator and discriminator)
+        # Trains the generator to fool the discriminator
+        self.criterion = nn.BCELoss()
+        self.optim_g = optim.Adam(self.generator.parameters(), lr=lr, betas=betas)
 
-            preview_frame = create_next_frame(model, pre_frame, unloader, device)
-            preview_frame.save(f'outputs/{epoch+1} output.jpg')
+    def train(self, epochs, batch_size=128, sample_interval=50):
+        # Load the dataset
+        transform = transforms.Compose([
+            transforms.Resize(28),
+            transforms.CenterCrop(28),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5], std=[0.5])
+        ])
+        train_dataset = datasets.MNIST('./data', train=True, download=True, transform=transform)
+        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
-    return train_loss, test_loss
+        for epoch in range(epochs):
 
+            # ---------------------
+            #  Train Discriminator
+            # ---------------------
 
-def evaluate(model, test_loader, l, device='cpu'):
-    testing_loss = 0
-    model = model.to(device)
-    for i, data in enumerate(test_loader):
-        inputs, outputs = data
-        inputs = inputs.to(device)
-        outputs = outputs.to(device)
+            for i, (imgs, _) in enumerate(train_loader):
+                imgs = imgs.to(self.device)
+                valid = torch.ones((imgs.size(0), 1)).to(self.device)
+                fake = torch.zeros((imgs.size(0), 1)).to(self.device)
 
-        with torch.no_grad():
-            outputs_bar = model(inputs)
-            loss = l(outputs_bar, outputs)
+                # Train the discriminator with real images
+                self.discriminator.zero_grad()
+                d_real = self.discriminator(imgs)
+                loss_d_real = self.criterion(d_real, valid)
+                loss_d_real.backward()
 
-            testing_loss += loss
+                # Train the discriminator with fake images
+                noise = torch.randn(imgs.size(0), self.latent_dim).to(self.device)
+                gen_imgs = self.generator(noise)
+                d_fake = self.discriminator(gen_imgs.detach())
+                loss_d_fake = self.criterion(d_fake, fake)
+                loss_d_fake.backward()
 
-    epoch_loss = testing_loss / len(test_loader)
-    return epoch_loss
+                loss_d = loss_d_real + loss_d_fake
+                self.optim_d.step()
 
+                # ---------------------
+                #  Train Generator
+                # ---------------------
 
-def create_next_frame(model, pre_frame, transform, device='cpu'):
-    model = model.to(device)
-    pre_frame = pre_frame.to(device)
-    next_frame = model(pre_frame) * 255.0
-    image = transform(next_frame[0].clone())
-    return image
+                self.generator.zero_grad()
+                d_fake_g = self.discriminator(gen_imgs)
+                loss_g = self.criterion(d_fake_g, valid)
+                loss_g.backward()
+                self.optim_g.step()
+
+                if i == len(train_loader) - 1:
+                    print("%d [D loss: %f] [G loss: %f]" % (epoch, loss_d.item(), loss_g.item()))
+
+            # If at save interval => save generated image samples
+            if epoch % sample_interval == 0:
+                self.sample_images(epoch)
+
+    def sample_images(self, epoch, nrow=5):
+        noise = torch.randn(nrow ** 2, self.latent_dim).to(self.device)
+        gen_imgs = self.generator(noise)
+        gen_imgs = (gen_imgs + 1) / 2
+        save_image(gen_imgs, f"images/{epoch}.png", nrow=nrow)
